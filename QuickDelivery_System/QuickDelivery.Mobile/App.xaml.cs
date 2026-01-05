c56971e82de5e0f201115069f94e9e250d6ee99f
ooirina <200990762+ooirina@users.noreply.github.com>
2026-01-05 21:41:23 +0200

    Initializare corecta solutie si proiecte

---
+++ QuickDelivery_System/QuickDelivery.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace QuickDelivery.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

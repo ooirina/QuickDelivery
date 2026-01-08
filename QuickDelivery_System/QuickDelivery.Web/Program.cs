568f503e66a5e9d5a497bebb88c5d071f71cb758
ooirina <200990762+ooirina@users.noreply.github.com>
2026-01-08 19:08:12 +0200

    Editare cod program.cs date din bd

--- QuickDelivery_System/QuickDelivery.Web/Program.cs
+++ QuickDelivery_System/QuickDelivery.Web/Program.cs
@@ -37,17 +37,17 @@ using (var scope = app.Services.CreateScope())
     if (!context.Categorie.Any())
     {
         context.Categorie.AddRange(
-            new Categorie { Nume = "Pizza", Iconita = "e50695_pizza.png" },
-            new Categorie { Nume = "Sushi", Iconita = "cee7e7_sushi.png" },
-            new Categorie { Nume = "Pui", Iconita = "c075a3_fried-chicken.png" },
-            new Categorie { Nume = "Burger", Iconita = "923b65_hamburger.png" },
-            new Categorie { Nume = "Salata", Iconita = "062a79_salad.png" },
-            new Categorie { Nume = "Supa", Iconita = "45eb74_soup-plate.png" },
-            new Categorie { Nume = "Desert", Iconita = "7db943_cake.png" },
-            new Categorie { Nume = "Peste", Iconita = "d9f4bb_fish-food.png" },
-            new Categorie { Nume = "Vita", Iconita = "131b6bc5_steak-medium.png" },
-            new Categorie { Nume = "Porc", Iconita = "66420f_pig-face-emoji.png" },
-            new Categorie { Nume = "Paste", Iconita = "b1afcc_emoji_u1f35d.png" }
+            new Categorie { Nume = "Pizza", Iconita = "41a45a4a-4820-4640-9326-9fad7de50695_pizza.png" },
+            new Categorie { Nume = "Sushi", Iconita = "7aa57afe-7e78-4277-a92c-f41559cee7e7_sushi.png" },
+            new Categorie { Nume = "Pui", Iconita = "80b79d8f-809d-4dfc-82ee-3de842c075a3_fried-chicken.png" },
+            new Categorie { Nume = "Burger", Iconita = "1cb30fe9-7558-4c1f-a97a-61250c923b65_hamburger.png" },
+            new Categorie { Nume = "Salata", Iconita = "998de229-ce67-42dd-bffe-8de307062a79_salad.png" },
+            new Categorie { Nume = "Supa", Iconita = "a19fd42d-35c4-4218-af4c-b7520845eb74_soup-plate.png" },
+            new Categorie { Nume = "Desert", Iconita = "7ac8efb5-3202-4e18-88a7-2f6abf7db943_cake.png" },
+            new Categorie { Nume = "Peste", Iconita = "5cb6fa0f-8596-4570-8963-64cf88d9f4bb_fish-food.png" },
+            new Categorie { Nume = "Vita", Iconita = "c18510a3-da4b-4d24-9649-060a131b6bc5_steak-medium.png" },
+            new Categorie { Nume = "Porc", Iconita = "b70b026e-2fef-4645-b6fd-7e6e8966420f_pig-face-emoji.png" },
+            new Categorie { Nume = "Paste", Iconita = "77687b7a-08f3-4c64-afaa-61e3f8b1afcc_emoji_u1f35d.png" }
         );
         context.SaveChanges();
     }
@@ -57,7 +57,7 @@ using (var scope = app.Services.CreateScope())
     {
         context.Restaurant.AddRange(
             new Restaurant { Nume = "Amadeus", Adresa = "Strada Motilor, Cluj-Napoca", ImagineUrl = "/images/35acdaa3-1dd5-43f5-b3b1-3cfed7f3e210_ghent-apr-28-exterior-view-of-the-famous-amadeus-restaurant-on-apr-28-2018-at-ghent-belgium-PJKJ5R.jpg" },
-            new Restaurant { Nume = "Marty", Adresa = "Strada Alexandru Vaida Voevod 53-55, Cluj-Napoca", ImagineUrl = "/images/2bcd603b-4ce1-89f5-5e67feb5af19_185677-marty-restaurant.jpg" },
+            new Restaurant { Nume = "Marty", Adresa = "Strada Alexandru Vaida Voevod 53-55, Cluj-Napoca", ImagineUrl = "/images/2bcd603b-47c1-4ce1-89f5-5e67feb5af19_185677-marty-restaurant.jpg" },
             new Restaurant { Nume = "Samsara", Adresa = "Strada Cardinal Iuliu Hossu 3, Cluj-Napoca", ImagineUrl = "/images/4fc560ae-4bcc-4af1-b2fc-eb7e9b89faf1_2018_10_samsara_catalinhladi_9618-big.jpg" },
             new Restaurant { Nume = "Meat Up", Adresa = "Strada Gheorghe Sincai 14, Cluj-Napoca", ImagineUrl = "/images/f545f59a-5c59-45cb-8b87-59faa9489169_12-big.jpg" },
             new Restaurant { Nume = "Roata", Adresa = "Strada Alexandru Ciurea 6, Cluj-Napoca", ImagineUrl = "/images/199dc2b1-37e7-4639-8f76-3e8873765175_roata-10668-big.jpg" },

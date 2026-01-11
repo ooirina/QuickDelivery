5e571bce701bb90e1def3b20482430934ab53f96
ooirina <200990762+ooirina@users.noreply.github.com>
2026-01-11 02:37:21 +0200

    Reparare erori la admin in categoria recenzii

--- QuickDelivery_System/QuickDelivery.Web/Models/Restaurant.cs
+++ QuickDelivery_System/QuickDelivery.Web/Models/Restaurant.cs
@@ -28,7 +28,7 @@ namespace QuickDelivery.Web.Models
         [JsonIgnore]
         public virtual ICollection<Produs>? Produse { get; set; }
 
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
     }
 }
